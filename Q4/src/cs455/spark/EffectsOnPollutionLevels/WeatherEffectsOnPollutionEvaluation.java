950f30b4357e01028460a478ef9c239ea4049341
Keith Rice <KRJuggling@gmail.com>
2018-04-24 23:30:33 -0400

    Weather base completed

--- Q4/src/cs455/spark/EffectsOnPollutionLevels/WeatherEffectsOnPollutionEvaluation.java
+++ Q4/src/cs455/spark/EffectsOnPollutionLevels/WeatherEffectsOnPollutionEvaluation.java
@@ -6,6 +6,8 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.*;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -28,6 +30,8 @@ public final class WeatherEffectsOnPollutionEvaluation
 			.builder()
 			.appName("WeatherEffectsOnPollution")
 			.getOrCreate();
+		SparkContext sc = spark.sparkContext();
+		JavaSparkContext jsc = JavaSparkContext.fromSparkContext(sc);
 
 		// read in the files (if passed a dir it will read all files in it)
 		//JavaRDD<String> pollutionLines = spark.read().textFile(args[0]).javaRDD();
@@ -40,32 +44,120 @@ public final class WeatherEffectsOnPollutionEvaluation
 		JavaRDD<ArrayList<String>> weatherTempData = weatherTempLines.map(new GetWeatherData());
 		JavaRDD<ArrayList<String>> weatherDewData = weatherDewLines.map(new GetWeatherData());
 		JavaRDD<ArrayList<String>> weatherWindData = weatherWindLines.map(new GetWeatherData());
-		weatherTempData.saveAsTextFile(args[2]+"1");
 		
 		//flatten the data so each entry is one time stamp
 		JavaRDD<String> weatherTempFlatData = weatherTempData.flatMap(new ExpandRawWeatherData());
 		JavaRDD<String> weatherDewFlatData = weatherDewData.flatMap(new ExpandRawWeatherData());
 		JavaRDD<String> weatherWindFlatData = weatherWindData.flatMap(new ExpandRawWeatherData());
-		weatherTempFlatData.saveAsTextFile(args[2]+"2");
 		
 		//rekey them based on the timestamp
 		JavaPairRDD<String, String> weatherTempKeyedData = weatherTempFlatData.mapToPair(new KeyWeatherData());
 		JavaPairRDD<String, String> weatherDewKeyedData = weatherDewFlatData.mapToPair(new KeyWeatherData());
 		JavaPairRDD<String, String> weatherWindKeyedData = weatherWindFlatData.mapToPair(new KeyWeatherData());
-		weatherTempKeyedData.saveAsTextFile(args[2]+"3");
 		
 		//finally combine all the weather data into one RDD
 		JavaPairRDD<String, Tuple2<String, String>> weatherDewAndWindRawData = weatherDewKeyedData.join(weatherWindKeyedData);
 		JavaPairRDD<String, Tuple2<String, Tuple2<String, String>>> weatherAllRawData = weatherTempKeyedData.join(weatherDewAndWindRawData);
 		JavaPairRDD<String, ArrayList<String>> weatherAllData = weatherAllRawData.mapToPair(new CombineWeatherData());
-		weatherDewAndWindRawData.saveAsTextFile(args[2]+"4");
-		weatherAllRawData.saveAsTextFile(args[2]+"5");
-		weatherAllData.saveAsTextFile(args[2]);
+		
+		//now convert the values to flags
+		//For temperature we want to base this on the average temperature
+		//for wind we want to base it on the average wind
+		//for rain we want to correlate temp and dew point
+		JavaRDD<ArrayList<String>> weatherAllDataValues = weatherAllData.values();
+		
+		ArrayList<String> weatherTotals = weatherAllDataValues.reduce(new TotalWeatherValues());
+		ArrayList<String> weatherAvgs = AverageWeatherData(weatherTotals);
+		List<ArrayList<String>> rddPrep = Arrays.asList(weatherAvgs);
+		
+		//convert it back to an RDD
+		JavaRDD<ArrayList<String>> weatherAvgsRdd = jsc.parallelize(rddPrep);
+		JavaPairRDD<String, ArrayList<String>> weatherAvgsRddDummyKey = weatherAvgsRdd.mapToPair(v -> new Tuple2<String, ArrayList<String>>("dummykey", v));
+		
+		//now associate the averages and determine the flags
+		JavaPairRDD<String, Tuple2<String, ArrayList<String>>> weatherAllDataDumyKey = weatherAllData.mapToPair(v -> new Tuple2<String, Tuple2<String, ArrayList<String>>>("dummykey", v));
+		JavaPairRDD<String, Tuple2<ArrayList<String>, Tuple2<String, ArrayList<String>>>> weatherAvgCombined = weatherAvgsRddDummyKey.join(weatherAllDataDumyKey);
+		JavaPairRDD<String, ArrayList<String>> weatherAllFlagData = weatherAvgCombined.mapToPair(new MakeWeatherDataFlags());
+		//outputs the weather data weatherAllFlagData.saveAsTextFile(args[2]);
 
 		// end the session
 		spark.stop();
 	}
 		
+	private static class MakeWeatherDataFlags implements PairFunction<Tuple2<String, Tuple2<ArrayList<String>, Tuple2<String, ArrayList<String>>>>, String, ArrayList<String>>
+	{
+		public Tuple2<String, ArrayList<String>> call (Tuple2<String, Tuple2<ArrayList<String>, Tuple2<String, ArrayList<String>>>> data)
+		{
+			//data._2()._1() is the averages
+			//data._2()._2()._2() is the current measurments
+			//data._2()._2()._1() is the key
+			ArrayList<String> newData = new ArrayList<String>();
+			
+			//for temperature just toggle high on and off based on the average
+			if (Double.parseDouble(data._2()._2()._2().get(0)) > Double.parseDouble(data._2()._1().get(0)))
+			{
+				newData.add("HOT");
+			}
+			else
+			{
+				newData.add("COLD");
+			}
+			
+			//for dewpoint check if its higher than the temperature
+			if (Double.parseDouble(data._2()._2()._2().get(1)) > Double.parseDouble(data._2()._2()._2().get(0)))
+			{
+				newData.add("RAINING");
+			}
+			else
+			{
+				newData.add("CLEAR");
+			}
+			
+			//for wind just toggle high on and off based on the average
+			if (Double.parseDouble(data._2()._2()._2().get(2)) > Double.parseDouble(data._2()._1().get(2)))
+			{
+				newData.add("HIGH");
+			}
+			else
+			{
+				newData.add("LOW");
+			}
+			
+			return new Tuple2<String, ArrayList<String>>(data._2()._2()._1(), newData);
+		}
+	}
+	
+	private static ArrayList<String> AverageWeatherData (ArrayList<String> data)
+	{
+		// data 0-2 is the pollution data, 3 is the count
+		
+		//Sum the data
+		ArrayList<String> averaged = new ArrayList<String>();
+		int lastIndex = data.size() - 1;
+		double count = Double.parseDouble(data.get(lastIndex));
+		for (int i = 0; i < lastIndex; i++)
+		{
+			averaged.add("" + Double.parseDouble(data.get(i)) / count);
+		}
+		return averaged;
+	}
+	
+	private static class TotalWeatherValues implements Function2<ArrayList<String>, ArrayList<String>, ArrayList<String>>
+	{
+		public ArrayList<String> call (ArrayList<String> data1, ArrayList<String> data2)
+		{
+			// data1 and data2 0-2 is the pollution data, 3 is the count
+
+			//Sum the data
+			ArrayList<String> combined = new ArrayList<String>();
+			for (int i = 0; i < data1.size(); i++)
+			{
+				combined.add("" + (Double.parseDouble(data1.get(i)) + Double.parseDouble(data2.get(i))));
+			}
+			return combined;
+		}
+	}
+	
 	private static class CombineWeatherData implements PairFunction<Tuple2<String, Tuple2<String, Tuple2<String, String>>>, String, ArrayList<String>>
 	{
 		public Tuple2<String, ArrayList<String>> call (Tuple2<String, Tuple2<String, Tuple2<String, String>>> data)
@@ -75,11 +167,11 @@ public final class WeatherEffectsOnPollutionEvaluation
 			newData.add(data._2()._1());
 			newData.add(data._2()._2()._1());
 			newData.add(data._2()._2()._2());
+			newData.add("1"); //add a count
 			
 			return new Tuple2<String, ArrayList<String>>(data._1(), newData);
 		}
 	}
-	
 		
 	private static class KeyWeatherData implements PairFunction<String, String, String>
 	{
@@ -98,8 +190,22 @@ public final class WeatherEffectsOnPollutionEvaluation
 	{
 		public Iterator<String> call (ArrayList<String> data)
 		{			
+		    ArrayList<String> flattened = new ArrayList<String>(data);
 			//flatten the data into the timestamp as keys
-			return data.iterator();
+			for (int i = 0; i < flattened.size(); i++)
+			{
+				//mae sure it is a valid number
+				try
+				{
+					Double.parseDouble(flattened.get(i).split(":")[3]);
+				}
+				catch (NumberFormatException nfe)
+				{
+					flattened.remove(i);
+					i--;
+				}
+			}
+			return flattened.iterator();
 		}
 	}
 	
